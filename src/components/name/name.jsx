0cc21d926abaac15e7d4ea8a002122b2644c3de0
LL1aMaH <48447371+LL1aMaH@users.noreply.github.com>
2020-06-03 13:11:16 +0300

    part2.2

---
+++ src/components/name/name.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './name.scss';
+
+const Name = () => (
+  <div className="Name">
+    <h2>Name Name</h2>
+    <div className="info">
+      <span>genre</span>
+      <div className="vl" />
+      <span>time</span>
+    </div>
+  </div>
+);
+
+export default Name;
--- src/pages/Back.jsx
+++ src/pages/Back.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import Search from '../components/search';
 import Button from '../components/button';
+import Name from '../components/name';
+
 import './Back.scss';
 
 export default function Back() {
   return (
     <div className="background">
+      <Name />
       <Search />
       <Button />
       <Button />
