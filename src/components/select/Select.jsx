bfd382556989c1b7355f50f1cea873ebe96ece85
LL1aMaH <48447371+LL1aMaH@users.noreply.github.com>
2020-08-22 16:11:15 +0300

    part3.6

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { typeGenre } from '../../config.data';
 import './Select.scss';
 
-const Select = ({ isClearable, onChange }) => (
-  <select
-    defaultValue="Genre"
-    onChange={onChange}
-    className="select"
-    isClearable={isClearable}
-  >
-    <option disabled>Genre</option>
-    {Object.keys(typeGenre).map((genre) => (
-      <option value={genre} key={genre}>
-        {genre[0].toUpperCase() + genre.slice(1)}
-      </option>
-    ))}
-  </select>
-);
+const Select = ({ isClearable, onChange }) => {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    setState(isClearable);
+  }, [isClearable]);
+  console.log(`isClearable = ${isClearable}`);
+  console.log(`state = ${state}`);
+
+  return (
+    <select defaultValue="Genre" onChange={onChange} className="select">
+      <option disabled>Genre</option>
+      {Object.keys(typeGenre).map((genre) => (
+        <option value={genre} key={genre}>
+          {genre[0].toUpperCase() + genre.slice(1)}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 export default Select;
